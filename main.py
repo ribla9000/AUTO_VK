764f93881e4f4ac3752e270344ca9d06d9ccc6b4
ribla9000 <2001rna.mail@mail.ru>
2023-08-02 06:45:35 +0300

    fixed

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
-from endpoints.gui import run_choose_prof
 from endpoints.check_req import check_req
 
 
 def main():
     try:
+        from endpoints.gui import run_choose_prof
         run_choose_prof()
     except:
         check_req()
-    
+        try:
+            from endpoints.gui import run_choose_prof
+            run_choose_prof()
+        except Exception as e:
+            print(e)
+
 
 if __name__ == '__main__':
     main()
